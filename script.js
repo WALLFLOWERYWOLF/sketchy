2738e9ad06b5b73963e1dfb3289ed8e785ad846c
WALLFLOWERYWOLF <121654472+WALLFLOWERYWOLF@users.noreply.github.com>
2025-09-25 10:54:51 +0530

    feat: add rainbow function
    This commit introduces a new Rainbow feature where it fills the cells
    with a random color creating a rainbow like effect.

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const container = document.querySelector(".container");
 const changeGridSizeBtn = document.querySelector(".change-grid-size");
+const rainbowBtn = document.querySelector(".rainbow");
+let pencilColor = "black";
+let bgColor = "white";
+let mode = "normal";
 let totalGridCells = 256;
 let numberOfCellsPerSide = 16;
+
+function setGridData() {
+  userInput = +prompt("Enter New Grid Size");
+  if (isNaN(userInput) || userInput > 100 || userInput < 1) {
+    alert("Please enter a valid number from 1 to 100");
+  } else {
+    numberOfCellsPerSide = userInput;
+    totalGridCells = userInput * userInput;
+  }
+}
 
 function drawGrid() {
   container.innerHTML = "";
@@ -16,23 +30,30 @@ function drawGrid() {
   }
 }
 
-function setGridData() {
-  userInput = +prompt("Enter New Grid Size");
-  if (isNaN(userInput) || userInput > 100 || userInput < 1) {
-    alert("Please enter a valid number from 1 to 100");
-  } else {
-    numberOfCellsPerSide = userInput;
-    totalGridCells = userInput * userInput;
-  }
+function rainbow() {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
 }
 
 container.addEventListener("mouseover", (e) => {
-  e.target.style.backgroundColor = "black";
+  switch (mode) {
+    case "normal":
+      e.target.style.backgroundColor = pencilColor;
+      break;
+    case "rainbow":
+      e.target.style.backgroundColor = rainbow();
+  }
 });
 
 changeGridSizeBtn.addEventListener("click", () => {
   setGridData();
   drawGrid();
+});
+
+rainbowBtn.addEventListener("click", () => {
+  mode = "rainbow";
 });
 
 drawGrid();
